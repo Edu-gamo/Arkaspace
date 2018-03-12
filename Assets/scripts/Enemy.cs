9646a8cb7ded562098142b93874f8e4a10de5412
Edu-gamo <eduard_gm94@hotmail.com>
2018-03-12 15:24:26 +0100

    Initial commit

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    
+    private float speed = 0.01f;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Movimiento descendente de la nave
+        this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - speed);
+
+    }
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    
+    private float speed = 0.1f;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Movimiento lateral de la nave
+        this.transform.position = new Vector2(this.transform.position.x + (Input.GetAxis("Horizontal") * speed), this.transform.position.y);
+
+    }
+
+    private void OnCollisionEnter2D (Collision2D other) {
+        if (other.gameObject.tag == "Enemy") {
+            Destroy(other.gameObject);
+        }
+    }
+}
